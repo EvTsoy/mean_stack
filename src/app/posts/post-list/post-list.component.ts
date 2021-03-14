a80c26d3f79568cc718b5633fe32b346bbf80553
Evgeniy Tsoy <EvTsoy@MacBook.local>
2021-03-14 18:58:22 +0500

    first commit

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,40 @@
+import { i18nMetaToJSDoc } from '@angular/compiler/src/render3/view/i18n/meta';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit, OnDestroy {
+
+  // posts = [
+  //   {title: 'First Post', content: 'This is the first post'},
+  //   {title: 'Second Post', content: 'This is the first post'},
+  //   {title: 'Third Post', content: 'This is the first post'},
+  //   {title: 'Last Post', content: 'This is the first post'},
+  // ];
+
+  posts: Post[] = [];
+  private postsSub: Subscription;
+
+  constructor(public postsService: PostsService) { }
+
+  ngOnInit(): void {
+    this.posts = this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListener()
+      .subscribe(
+        (posts: Post[]) => {
+          this.posts = posts;
+        }
+      );
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
+  }
+
+}
